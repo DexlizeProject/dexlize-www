f002a294b59d0d22ab1a7ff6cf82d84f24922ecd
Robbie <10761269+exrobbie@users.noreply.github.com>
2018-08-13 13:10:31 +0800

    update token number

--- src/components/IndexPage/SectionToken/index.js
+++ src/components/IndexPage/SectionToken/index.js
@@ -25,12 +25,12 @@ export default class SectionToken extends React.Component{
                         </div>
                         <div className="token-transform">
                             <ul>
-                                <li>x  <FormattedMessage id="token.totalTokenIssued"/> = 100M DXL</li>
-                                <li>1  <FormattedMessage id="token.sale"/>  (23%) = 23M DXL</li>
-                                <li>2  <FormattedMessage id="token.team"/>  (20%) = 20M DXL</li>
-                                <li>3  <FormattedMessage id="token.foundation"/>  (22%) = 22M DXL</li>
-                                <li>4  <FormattedMessage id="token.systemIncentive"/>  (30%) = 30M DXL</li>
-                                <li>5  <FormattedMessage id="token.communityReward"/>  (5%) = 5M DXL</li>
+                                <li>x  <FormattedMessage id="token.totalTokenIssued"/> = 1B DXL</li>
+                                <li>1  <FormattedMessage id="token.sale"/>  (23%) = 230M DXL</li>
+                                <li>2  <FormattedMessage id="token.team"/>  (20%) = 200M DXL</li>
+                                <li>3  <FormattedMessage id="token.foundation"/>  (22%) = 220M DXL</li>
+                                <li>4  <FormattedMessage id="token.systemIncentive"/>  (30%) = 300M DXL</li>
+                                <li>5  <FormattedMessage id="token.communityReward"/>  (5%) = 50M DXL</li>
                             </ul>
                         </div>
                     </div>
