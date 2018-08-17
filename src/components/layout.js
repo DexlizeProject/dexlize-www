452cf1eff21f17b7f2b832d3d58fcfa7d66cfdf9
litianao <exrobbie@gmail.com>
2018-08-17 16:15:18 +0800

    added favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import {injectIntl} from 'react-intl'
+import {injectIntl, FormattedMessage} from 'react-intl'
 import Header from './Header'
 
 const Layout = ({children, data, intl}) => (
     <div>
-        <Helmet
-            title={intl.formatMessage({id: 'title'})}
-            meta={[
-                {name: 'description', content: intl.formatMessage({id: 'welcome'})},
-                {
-                    name: 'keywords',
-                    content: 'gatsby, i18n, react-intl, multi language, localization',
-                },
-            ]}
-        />
+        <Helmet>
+            <link rel="shortcut icon" href="/static/favicon.ico"/>
+            <title><FormattedMessage id="title"/></title>
+            <meta name="description" content={intl.formatMessage({id: 'welcome'})}/>
+            <meta name="keywords" content="dexlize, bitcoin"/>
+        </Helmet>
         <Header siteTitle={intl.formatMessage({id: 'title'})}/>
         <div>
             {children}
