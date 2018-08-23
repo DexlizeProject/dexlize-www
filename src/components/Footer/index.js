96a18cba3849850aaebe8ace4d305799701d3b1f
litianao <exrobbie@gmail.com>
2018-08-23 17:53:13 +0800

    updated footer share group

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,13 +9,20 @@ export default class Footer extends React.Component{
                     <span className="icon icon-logo-light"/>
                     <div className="footer-share-copyright">
                         <div className="footer-share">
-                            <span className="icon icon-twitter"/>
-                            <span className="icon icon-discord"/>
-                            <span className="icon icon-wechat"/>
-                            <span className="icon icon-telegram"/>
-                            <span className="icon icon-github"/>
-                            <span className="icon icon-weibo"/>
-                            <span className="icon icon-facebook"/>
+                            <a href="https://medium.com/@Dexlize1">
+                                <span className="icon icon-medium"/>
+                            </a>
+                            <a href="https://twitter.com/Dexlize1">
+                                <span className="icon icon-twitter"/>
+                            </a>
+                            {/*<span className="icon icon-discord"/>*/}
+                            {/*<span className="icon icon-wechat"/>*/}
+                            {/*<span className="icon icon-telegram"/>*/}
+                            <a href="https://github.com/Dexlize">
+                                <span className="icon icon-github"/>
+                            </a>
+                            {/*<span className="icon icon-weibo"/>*/}
+                            {/*<span className="icon icon-facebook"/>*/}
                         </div>
                         <div className="footer-copyright">
                             Copyright © ️2018. All Rights Reserved By Dexlize Fondation PTE. LTD.
